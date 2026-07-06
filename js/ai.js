c0ac4baa06c47dd90e8cecebc54ab98a2e493906
silvadanielusa2014-a11y <silva.daniel.usa2014@gmail.com>
2026-07-06 17:34:52 -0500

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -1,35 +1,47 @@
-import * as webllm from "https://esm.run/@mlc-ai/web-llm";
+import * as webllm from
+"https://esm.run/@mlc-ai/web-llm";
+
 
 let engine;
 
 
-export async function startAI() {
+export async function startAI(){
 
-    engine = await webllm.CreateMLCEngine(
+    engine =
+    await webllm.CreateMLCEngine(
+
         "Llama-3.2-1B-Instruct-q4f16_1"
+
     );
 
 }
 
 
-export async function askAI(message) {
+export async function askAI(
+    systemPrompt,
+    messages
+){
 
-    const result = await engine.chat.completions.create({
+    const response =
+    await engine.chat.completions.create({
 
-        messages: [
+        messages:[
+
             {
-                role: "system",
-                content: "You are a helpful offline AI assistant."
+                role:"system",
+                content:systemPrompt
             },
-            {
-                role: "user",
-                content: message
-            }
+
+            ...messages
+
         ]
 
     });
 
 
-    return result.choices[0].message.content;
+    return response
+        .choices[0]
+        .message
+        .content;
 
 }
