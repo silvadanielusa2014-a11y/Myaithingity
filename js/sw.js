c50ef28e7515d9d126da60230fb3755e2edcd443
silvadanielusa2014-a11y <silva.daniel.usa2014@gmail.com>
2026-07-07 09:31:48 -0500

    Create sw.js

---
+++ js/sw.js
@@ -0,0 +1,128 @@
+const CACHE_NAME = "myaithingity-shell-v1";
+
+
+const APP_FILES = [
+
+    "./",
+
+    "./index.html",
+
+    "./character-creator.html",
+
+    "./manifest.json",
+
+
+    "./css/style.css",
+
+    "./css/creator.css",
+
+
+    "./js/app.js",
+
+    "./js/ai.js",
+
+    "./js/aiManager.js",
+
+    "./js/assetLoader.js",
+
+    "./js/characterManager.js",
+
+    "./js/characters.js",
+
+    "./js/creator.js",
+
+    "./js/memory.js",
+
+    "./js/rules.js",
+
+    "./js/ui.js"
+
+];
+
+
+
+self.addEventListener(
+    "install",
+    event => {
+
+        event.waitUntil(
+
+            caches.open(CACHE_NAME)
+
+            .then(cache => {
+
+                return cache.addAll(
+                    APP_FILES
+                );
+
+            })
+
+        );
+
+    }
+
+);
+
+
+
+self.addEventListener(
+    "activate",
+    event => {
+
+        event.waitUntil(
+
+            caches.keys()
+
+            .then(keys => {
+
+                return Promise.all(
+
+                    keys.map(key => {
+
+                        if (
+                            key !== CACHE_NAME
+                        ) {
+
+                            return caches.delete(
+                                key
+                            );
+
+                        }
+
+                    })
+
+                );
+
+            })
+
+        );
+
+    }
+
+);
+
+
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+
+        event.respondWith(
+
+            caches.match(
+                event.request
+            )
+
+            .then(cached => {
+
+                return cached ||
+                    fetch(event.request);
+
+            })
+
+        );
+
+
+    }
+);
