df896f09219e2b50a236b89884b92815abb64530
silvadanielusa2014-a11y <silva.daniel.usa2014@gmail.com>
2026-07-07 15:52:27 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,106 +1,147 @@
-const CACHE_NAME = "myaithingity-shell-v1";
+const CACHE_NAME =
+    "myaithingity-shell-v2";
+
 
 
 const APP_FILES = [
 
+
     "./",
 
+
     "./index.html",
+
     "./character-creator.html",
+
     "./ai-models.html",
+
 
     "./manifest.json",
 
 
+
     "./css/style.css",
+
     "./css/creator.css",
 
 
+
+
     "./js/app.js",
+
+    "./js/router.js",
+
+    "./js/ui.js",
+
+
+
     "./js/ai.js",
+
+    "./js/webllm.js",
+
     "./js/aiManager.js",
+
     "./js/aiModels.js",
+
     "./js/modelManager.js",
+
+
+
     "./js/assetLoader.js",
+
     "./js/characterManager.js",
+
     "./js/characters.js",
+
     "./js/creator.js",
+
     "./js/memory.js",
+
     "./js/rules.js",
-    "./js/ui.js"
+
+
+
+
+    "./views/chat.html",
+
+    "./views/characters.html",
+
+    "./views/models.html",
+
+    "./views/settings.html"
+
+
 
 ];
 
 
 
+
+
+
 self.addEventListener(
+
     "install",
+
     event => {
+
 
         event.waitUntil(
 
-            caches.open(CACHE_NAME)
 
-            .then(async cache => {
+            caches.open(
+                CACHE_NAME
+            )
 
-                for (const file of APP_FILES) {
+            .then(
 
-                    try {
-
-                        await cache.add(file);
-
-                    }
-
-                    catch(error) {
-
-                        console.warn(
-                            "Failed to cache:",
-                            file
-                        );
-
-                    }
-
-                }
-
-            })
-
-        );
-
-    }
-
-);
+                async cache => {
 
 
+                    for (
 
-self.addEventListener(
-    "activate",
-    event => {
+                        const file
 
-        event.waitUntil(
+                        of APP_FILES
 
-            caches.keys()
+                    ) {
 
-            .then(keys => {
 
-                return Promise.all(
+                        try {
 
-                    keys.map(key => {
 
-                        if(key !== CACHE_NAME){
-
-                            return caches.delete(
-                                key
+                            await cache.add(
+                                file
                             );
+
 
                         }
 
-                    })
 
-                );
+                        catch(error) {
 
-            })
+
+                            console.warn(
+
+                                "Failed to cache:",
+                                file
+
+                            );
+
+
+                        }
+
+
+                    }
+
+
+                }
+
+            )
+
 
         );
+
 
     }
 
@@ -108,23 +149,114 @@ self.addEventListener(
 
 
 
+
+
+
+
 self.addEventListener(
-    "fetch",
+
+    "activate",
+
     event => {
+
+
+        event.waitUntil(
+
+
+            caches.keys()
+
+            .then(
+
+                keys => {
+
+
+                    return Promise.all(
+
+                        keys.map(
+
+                            key => {
+
+
+                                if(
+
+                                    key !== CACHE_NAME
+
+                                ){
+
+                                    return caches.delete(
+                                        key
+                                    );
+
+                                }
+
+
+                            }
+
+                        )
+
+                    );
+
+
+                }
+
+            )
+
+
+        );
+
+
+    }
+
+);
+
+
+
+
+
+
+
+
+self.addEventListener(
+
+    "fetch",
+
+    event => {
+
 
         event.respondWith(
 
+
             caches.match(
+
                 event.request
+
             )
-            .then(cached => {
 
-                return cached ||
-                    fetch(event.request);
+            .then(
 
-            })
+                cached => {
+
+
+                    if(cached){
+
+                        return cached;
+
+                    }
+
+
+
+                    return fetch(
+                        event.request
+                    );
+
+
+                }
+
+            )
+
 
         );
+
 
     }
 
